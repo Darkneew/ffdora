225334f6692650f7c58b0bf0f365900f12bee761
lautla <114079284+lautla@users.noreply.github.com>
2023-05-19 17:27:19 +0200

    Update tty.h

--- kernel/include/kernel/tty.h
+++ kernel/include/kernel/tty.h
@@ -12,7 +12,6 @@ void terminal_setcolor(uint8_t);
 void terminal_putchar(char c);
 void terminal_write(const char* data, size_t size);
 void terminal_writestring(const char* data);
-bool get_cff(void);
 char* get_clavier(void);
 char* get_perso(int rand,int stat);
 size_t get_terminal_row(void);
